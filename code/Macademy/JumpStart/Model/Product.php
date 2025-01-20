2c6874f966fc50e6f99c58af74f79d02b8e7d5ca
virendra patil <virendra.patil@hbwsl.com>
2025-01-20 15:43:56 +0530

    constructor property promotion

--- code/Macademy/JumpStart/Model/Product.php
+++ code/Macademy/JumpStart/Model/Product.php
@@ -6,14 +6,11 @@ namespace Macademy\Jumpstart\Model;
 
 class Product
 {
-    private Category $category;
+    function __construct(
+        private Category $category,
+    ) {}
 
-    function __construct(Category $category)
-    {
-        $this->category = $category;
-    }
-
-    function getCategoryName(): string
+    function getCategoryName()
     {
         return $this->category->getName();
     }
